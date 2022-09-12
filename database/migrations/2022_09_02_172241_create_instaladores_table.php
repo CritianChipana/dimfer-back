a2de76eea711a7eb5c21bf76e0906fa3337b13cd
CristianChipana <cristianchipanahuaman@gmail.com>
2022-09-12 16:40:34 -0500

    tabla de instaladores corregido

--- database/migrations/2022_09_02_172241_create_instaladores_table.php
+++ database/migrations/2022_09_02_172241_create_instaladores_table.php
@@ -15,17 +15,17 @@ class CreateInstaladoresTable extends Migration
     {
         Schema::create('instaladores', function (Blueprint $table) {
             $table->id();
-            $table->string('nombre');
-            $table->string('primer_apellido');
-            $table->string('segundo_apellido');
-            $table->string('departamento');
-            $table->string('provincia');
-            $table->string('distrito');
+            $table->string('nombre')->nullable();
+            $table->string('primer_apellido')->nullable();
+            $table->string('segundo_apellido')->nullable();
+            $table->string('departamento')->nullable();
+            $table->string('provincia')->nullable();
+            $table->string('distrito')->nullable();
             $table->string('direccion')->nullable();
-            $table->string('dni');
-            $table->string('celular');
-            $table->string('correo');
-            $table->string('especializacion');
+            $table->string('dni')->nullable();
+            $table->string('celular')->nullable();
+            $table->string('correo')->nullable();
+            $table->string('especializacion')->nullable();
             $table->string('foto')->nullable();
             $table->timestamps();
         });
